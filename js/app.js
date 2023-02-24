3cb90de8177fa7f2b20419bf660d5489e35a9c8b
Ridoy Sharif <developersridoy@gmail.com>
2023-02-24 10:00:30 +0600

    loadCountryDetails function created

--- js/app.js
+++ js/app.js
@@ -16,10 +16,19 @@ const displayCountry = (countries) => {
          <img src="${country.flags.png}"/>
       </div>
       <h3>Name: ${country.name.common}</h3>
-      <button>Details</button>
+      <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
       `
       allCountry.appendChild(displayCountryDiv)
-      console.log(country)
+      // console.log(country)
    })
 }
+// country details print
+const countryDetailsPrintId = document.getElementById("country-details");
+const loadCountryDetails = (code) => {
+   const url = `https://restcountries.com/v3.1/alpha/${code}`
+   fetch(url)
+      .then(res => res.json())
+      .then(data => console.log(data[0]))
+}
+
 loadApi()
